f1b577af2fb9eb108abb1d1dfd6ae09e41fff388
Paul Harrington <paul@Pauls-MacBook-Air.local>
2023-06-02 22:48:48 +0100

    WIP

--- src/components/ContactFormModal.js
+++ src/components/ContactFormModal.js
@@ -11,13 +11,56 @@ const ContactFormModal = () => {
       }
     >
       <div className="modal-container">
-        <h3>contact form content goes here!</h3>
         <button
           className="close-modal-btn"
           onClick={() => setToggleContactFormModal(false)}
         >
           <GrFormClose />
         </button>
+        <div>
+          <form method="POST" className="form" data-netlify="true">
+            {/* first name */}
+            <div className="form-row">
+              <input
+                type="text"
+                placeholder="First Name"
+                className="form-input"
+                id="firstName"
+                name="firstName"
+              />
+            </div>
+            {/* last name */}
+            <div className="form-row">
+              <input
+                type="text"
+                placeholder="Last Name"
+                className="form-input"
+                id="lastName"
+                name="lastName"
+              />
+            </div>
+            {/* email */}
+            <div className="form-row">
+              <input
+                type="email"
+                placeholder="Email Address"
+                className="form-input"
+                id="email"
+                name="email"
+              />
+            </div>
+            {/* Netlify spam filtering */}
+            <div className="form-row">
+              <div data-netlify-recaptcha="true"></div>
+            </div>
+            <button
+              type="submit"
+              className="btn hero-btn hero-btn__contact-form"
+            >
+              Find your Perfect Match!
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
