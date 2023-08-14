6ca688f41939d4e63306224e528857e4ec20b60e
Paul Harrington <paul@Pauls-MacBook-Air.local>
2023-08-14 10:04:20 +0100

    WIP

--- src/components/FormAddress.js
+++ src/components/FormAddress.js
@@ -8,7 +8,7 @@ const AddressForm = ({ street, city, county, postcode, updateFields }) => {
         style={{
           position: 'absolute',
           width: '100%',
-          top: '-17vw',
+          top: '-9.56vh',
         }}
       >
         Your Address
